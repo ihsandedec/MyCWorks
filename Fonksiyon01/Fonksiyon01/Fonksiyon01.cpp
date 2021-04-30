3fc3a3e383c4bb8219d823539bbf54ebe911b9e9
ihsandedec <ddcihsan@gmail.com>
2021-04-30 04:08:03 +0300

    çalışma01 eklendi

---
+++ Fonksiyon01/Fonksiyon01/Fonksiyon01.cpp
@@ -0,0 +1,38 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+void karesinihesapla(int sayi)
+{
+    int kare = sayi * sayi;
+    cout << "sayinin karesi = " << kare << endl;
+}
+void merhabadunya()
+{
+    cout << "HEllo World ?" << endl;
+}
+int sayilartoplami(int a, int b)
+{
+    int toplam = 0;
+    for (int i = a; i < b; i++)
+    {
+        toplam = toplam + i;
+    }
+    cout << "sayilar arasi toplam =" << toplam - 1 << endl;
+    return toplam;
+}
+int main()
+{
+    merhabadunya();
+    cout << "karesi hesaplanacak sayiyi giriniz :";
+    int sayi1, sayi2;
+    cin >> sayi1;
+    karesinihesapla(sayi1);
+    cout << " sayi1 giriniz :";
+    cin >> sayi1;
+    cout << " sayi2 giriniz :";
+    cin >> sayi2;
+    sayilartoplami(sayi1, sayi2);
+    return 0;
+}
---
+++ Fonksiyon02/Fonksiyon02/Fonksiyon02.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+
+void yıldız1(int sayi)
+{
+    int satiruzunluk = sayi * 2 - 1;
+    for (int i = 0; i < sayi; i++)
+    {
+        for (int j = 0; j < satiruzunluk; j++)
+        {
+
+            if (j<i || j>satiruzunluk - i - 1)
+            {
+                cout << " ";
+            }
+            else
+            {
+                cout << "*";
+            }
+
+        }
+        cout << endl;
+    }
+}
+void yıldız2(int sayi)
+{
+    int satıruznluk = 2.5 * sayi;
+    for (int i = 0; i < sayi; i++)
+    {
+        for (int j = 0; j < satıruznluk; j++)
+        {
+            if (j > sayi - 1 && j < sayi + sayi / 2)
+            {
+                cout << " ";
+            }
+            else
+            {
+                cout << "*";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    yıldız1(6);
+    cout << endl;
+    yıldız2(4);
+    return 0;
+}
